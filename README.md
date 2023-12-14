33d9197a62b21daccc2ba4da43ff2037f34ff4f7
Dmytro Petyshyn <dev6012@gmail.com>
2023-12-14 16:40:44 +0200

    simple app

--- README.md
+++
@@ -1,7 +0,0 @@
-To test DASHBOARD you can use admin DEMO ACC:
-
-login:
-test@gmail.com
-
-password:
-testroot
--- app/page.jsx
+++
@@ -1,7 +0,0 @@
-import { Box } from '@chakra-ui/react';
-
-const Homepage = () => {
-  return <Box bgColor={'teal.300'}>Homepage</Box>;
-};
-
-export default Homepage;
